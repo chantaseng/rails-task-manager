0df93f073513771e80b12b23e89b105a50d79f17
William Chan <chan.william.ts@gmail.com>
2023-05-23 23:20:10 -0400

    cant submit edit form

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,4 +12,25 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def upgrade
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
